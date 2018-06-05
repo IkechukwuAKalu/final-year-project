9004f1cbc90b23fd680cea722315d53acb86cd7d
Ikechukwu .A. Kalu <ikechukwuakalu@gmail.com>
2018-06-05 16:59:49 +0100

    Add Naive Bayes classifier to the filter spam

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ $(document).ready(() => {
 });
 
 function initChat(user) {
-    const socket = io();
+    let socket = io();
 
     socket.on('socket_message', (data) => {
         let msgItem = `<div class='col-md-12 col-xs-12'>
@@ -34,11 +34,12 @@ function initChat(user) {
 
     socket.on('new_message', (data) => {
         let bg = data.sender === user ? 'green' : 'purple';
+        if (data.spam) bg = 'red'; // give it a red background for spams
         let floatDiv = data.sender === user ? 'right' : 'left';
         let msgItem = `<div class='col-md-12 col-xs-12'>
             <div style='padding:5px 10px 5px 10px;background:${bg};color:#fff;border-radius:15px;margin-bottom:10px;float:${floatDiv};'>
                 <p>
-                    <span>${data.msg}</span><br>
+                    <span>${(data.spam ? '<-- spam blocked -->' : data.message)}</span><br>
                     <i style='font-size:12px;float:right;padding:0px;'>- ${data.sender}</i>
                     <u class='text-center mark_spam' style='float:left;font-size:11px;cursor:pointer;'>mark as spam</u>
                     <div style='clear:both;'></div>
@@ -49,14 +50,15 @@ function initChat(user) {
 
         // This has to be in the context of the defining function
         $('.mark_spam').on('click', (e) => {
-            let msg = e.target.parentElement.children[0].textContent.toString();
-            alert(msg)
+            let message = e.target.parentElement.children[0].textContent.toString();
+            socket.emit('mark_spam', { message });
+            alert('marked as spam');
         });
     });
 
     $('#send_msg').on('click', (e) => {
-        let msg = $('#message').val().trim();
-        if (! msg) return alert('Enter a message');
-        else socket.emit('send_message', { msg, sender: user });
+        let message = $('#message').val().trim();
+        if (! message) return alert('Enter a message');
+        else socket.emit('send_message', { message, sender: user });
     });
 }
--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const NaiveBayes = require('node-naive-bayes');
+
+let naiveBayes = new NaiveBayes();
+console.log('...starting classifier training...');
+naiveBayes.trainFromFile('./training_data.txt');
+console.log('...training finished...');
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('server now listening...');
@@ -15,11 +21,19 @@ app.get('/', (req, res, next) => {
 
 io.on('connection', (socket) => {
     showSocketMsg(socket, 'A User has joined the conversation');
+
     socket.on('send_message', (data) => {
+        data.spam = naiveBayes.classify(data.message, 'unknown') === 'bad';
         io.emit('new_message', data);
     });
+
     socket.on('disconnect', (e) => {
         showSocketMsg(socket, 'A User has left the conversation');
+    });
+
+    socket.on('mark_spam', (data) => {
+        console.log(`Marking "${data.message}" as spam`);
+        naiveBayes.trainInline(data.message, 'bad');
     });
 });
 
